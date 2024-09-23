dbdb85fdd5d608da4a4f9c3f8f61988fb84c4538
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-09-23 20:11:24 -0300

    Finalização do projeto

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,31 @@
 import styles from "./home.module.css"
+import React, { useState } from "react";
 
 export default function Home() {
+    const [images, setImages] = useState(5);
+
+    const addImage = () => {
+        setImages(images + 1);
+    };
+
+    const removeImage = () => {
+        if (images > 0) setImages(images - 1);
+    };
+
     return (
-        <div>
-            <h1>Template</h1>
+        <div className={styles.container}>
+            <div className={styles.controls}>
+                <button onClick={addImage}>+</button>
+                <span>{images}</span>
+                <button onClick={removeImage}>-</button>
+            </div>
+            <div className={styles.gallery}>
+                {Array.from({ length: images }).map((_, index) => (
+                    <div key={index} className={styles.image}>
+                        <p>100 x 100</p>
+                    </div>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
